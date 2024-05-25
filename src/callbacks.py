856484eef769c3249aeb2ea09830e8bc4d718687
milosz-l <milosz.lopatto@gmail.com>
2024-05-25 20:41:00 +0000

    added wandb model checkpoint

--- src/callbacks.py
+++ src/callbacks.py
@@ -1,9 +1,8 @@
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 import pytorch_lightning as pl
 
-
 def get_callbacks(callback_config):
-    early_stop_callback = pl.callbacks.EarlyStopping(
+    early_stop_callback = EarlyStopping(
         monitor='val_loss',
         min_delta=0.0,
         patience=10,
@@ -12,10 +11,18 @@ def get_callbacks(callback_config):
     )
 
     checkpoint_callback = ModelCheckpoint(
-        filename='train',
+        filename='best-checkpoint',
         save_top_k=1,
         verbose=True,
         monitor='val_loss',
         mode='min',
     )
-    return [early_stop_callback, checkpoint_callback]
+
+    periodic_checkpoint_callback = ModelCheckpoint(
+        filename='checkpoint-{epoch:02d}-{val_loss:.2f}',
+        save_top_k=-1,  # Save all checkpoints
+        every_n_epochs=5,  # Save every 5 epochs
+        verbose=True,
+    )
+
+    return [early_stop_callback, checkpoint_callback, periodic_checkpoint_callback]
