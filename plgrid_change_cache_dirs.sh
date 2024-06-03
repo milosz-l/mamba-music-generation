89d5a199891df4fea35916a4832e19d4a0effe06
milosz-l <milosz.lopatto@gmail.com>
2024-06-03 11:15:19 +0200

    added script for changing cache dir outside of home dir (plgrid requirement)

---
+++ plgrid_change_cache_dirs.sh
@@ -0,0 +1,4 @@
+export WANDB_CACHE_DIR=/net/tscratch/people/$USER/.cache/wandb
+export POETRY_CACHE_DIR=/net/tscratch/people/$USER/.cache/pypoetry
+export PIP_CACHE_DIR=/net/tscratch/people/$USER/.cache/pip
+# export TORCH_HOME=/net/tscratch/people/$USER/.cache/torch
