b9537c7653907e3b77a2d7b2911d16d9601d8b00
heh3temp <jan.retkowski.stud@pw.edu.pl>
2024-06-04 23:53:10 +0200

    add dataset chunk preprocessing

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -4,6 +4,7 @@ from miditok import REMI, MIDILike, TSD, Structured, CPWord, Octuple, MuMIDI, MM
 from miditok.pytorch_data import DatasetMIDI, DataCollator
 from torch.utils.data import random_split
 from omegaconf import DictConfig
+from miditok.pytorch_data import split_files_for_training
 
 TOKENIZR_MAPPING = {
     "remi": REMI,
@@ -27,7 +28,8 @@ def get_tokenized_dataset(config: DictConfig):
     dataset_path = Path(config.data.path) / config.data.dataset_name
     tokenizer_path = get_tokenizer_path(config)
 
-    midi_paths = list(dataset_path.glob("**/*.mid*"))
+    # midi_paths = [path.resolve() for path in list(dataset_path.glob("**/*.mid*"))]
+    midi_paths = [Path('data/maestro/maestro-v3.0.0/2018/MIDI-Unprocessed_Schubert7-9_MID--AUDIO_16_R2_2018_wav.midi').resolve()]
     if not tokenizer_path.exists():
         tokenizer = TOKENIZR_MAPPING[config.data.tokenizer.type.lower()]()
         tokenizer.train(vocab_size=config.model.vocab_size,
@@ -37,9 +39,17 @@ def get_tokenized_dataset(config: DictConfig):
     else:
         tokenizer = TOKENIZR_MAPPING[config.data.tokenizer.type.lower()](params=tokenizer_path)
 
+    dataset_chunks_dir = dataset_path / Path('midi_chunks')
+
+    split_files_for_training(
+        files_paths=midi_paths,
+        tokenizer=tokenizer,
+        save_dir=dataset_chunks_dir,
+        max_seq_len=config.data.max_seq_len,
+    )
 
     dataset = DatasetMIDI(
-        files_paths=midi_paths,
+        files_paths=list(dataset_chunks_dir.glob("**/*.mid*")),
         tokenizer=tokenizer,
         max_seq_len=config.data.max_seq_len,
         bos_token_id=tokenizer["BOS_None"],
@@ -53,6 +63,7 @@ def get_tokenized_dataset(config: DictConfig):
 
     train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
     return train_dataset, val_dataset, collator
+    # return dataset, dataset, collator
 
 
 def load_pretrained_tokenizer(config: DictConfig):
